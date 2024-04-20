897117ef7425b2444bd422ef9007505a2b1919a6
Stephen Lance Alcoriza <a.stephenlance@gmail.com>
2024-04-20 08:21:21 +0800

    Removed unecessary lines.

--- osu!BL/Verify.cs
+++ osu!BL/Verify.cs
@@ -12,16 +12,10 @@ namespace osu_BL
             return result.userName != null ? true : false;
         }
 
-        public List<BeatMap> maplist = new List<BeatMap>();
         public List<BeatMap> GetBeatMaps()
         {
             SongMap songMap = new SongMap();
             return songMap.GetBeatMaps();
-        }
-
-        public void addSong(BeatMap song)
-        {
-            maplist.Add(song);
         }
     }
 }
